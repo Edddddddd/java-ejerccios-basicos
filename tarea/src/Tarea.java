d395db4f7ba8366b719f8c502e9524358aac5ad4
Eduardo Silva <eduardo.cristobal.silva@gmail.com>
2022-05-10 10:46:10 -0400

    first commit

---
+++ tarea/src/Tarea.java
@@ -0,0 +1,17 @@
+
+// Tarea
+
+/***
+ *
+ * Generar un flujo simple -- un arreglo o lista
+ * Debe manejar algun tipo de numerico
+ * Debe tiene tener al menos un if - else
+ * Debe tener una lista y un loop
+ * Loop: while, for, etc.
+ *
+ *
+ *
+ */
+
+public class Tarea {
+}
